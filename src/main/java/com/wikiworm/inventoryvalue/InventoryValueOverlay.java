eb3930f750b933c37a0c39a4a550ee85e8ad97cd
WikiWorm <wikiwormdaily@gmail.com>
2022-07-12 11:19:29 -0700

    Adding option to calculate profit
    new config option allows users to optionally display profit for the current inventory "session" and bank value change.

--- src/main/java/com/wikiworm/inventoryvalue/InventoryValueOverlay.java
+++ src/main/java/com/wikiworm/inventoryvalue/InventoryValueOverlay.java
@@ -27,6 +27,7 @@
  */
 package com.wikiworm.inventoryvalue;
 
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.LineComponent;
@@ -40,17 +41,21 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 
-
+@Slf4j
 public class InventoryValueOverlay extends Overlay
 {
-    private Long inventoryValue;
+    private Long inventoryValue = 0L;
+    private Long startingValue = Long.MIN_VALUE;
+    private Long profitInvValue = 0L;
+    private Long profitBankValue = 0L;
+    private Color profitInvColor = Color.GREEN;
+    private Color profitBankColor = Color.GREEN;
     private final InventoryValueConfig inventoryValueConfig;
     private final PanelComponent panelComponent = new PanelComponent();
 
     @Inject
     private InventoryValueOverlay(InventoryValueConfig config) {
         setPosition(OverlayPosition.ABOVE_CHATBOX_RIGHT);
-        this.inventoryValue = 0L;
         this.inventoryValueConfig = config;
     }
 
@@ -58,6 +63,8 @@ public class InventoryValueOverlay extends Overlay
     public Dimension render(Graphics2D graphics) {
         String titleText = "Inventory Value";
         String valueString = inventoryValueConfig.useHighAlchemyValue() ? "HA Price:" : "GE Price:";
+        String profitInvString = "Profit (inv):";
+        String profitBankString = "Profit (banked):";
 
         panelComponent.getChildren().clear();
 
@@ -73,13 +80,44 @@ public class InventoryValueOverlay extends Overlay
 
         panelComponent.getChildren().add(LineComponent.builder()
                 .left(valueString)
+                .leftColor(Color.WHITE)
                 .right(QuantityFormatter.quantityToStackSize(inventoryValue))
+                .rightColor(Color.YELLOW)
                 .build());
+
+        if(inventoryValueConfig.displayProfit()) {
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(profitInvString)
+                    .leftColor(Color.WHITE)
+                    .right(QuantityFormatter.quantityToStackSize(profitInvValue))
+                    .rightColor(profitInvColor)
+                    .build());
+        }
+
+        if(inventoryValueConfig.displayProfit()) {
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(profitBankString)
+                    .leftColor(Color.WHITE)
+                    .right(QuantityFormatter.quantityToStackSize(profitBankValue))
+                    .rightColor(profitBankColor)
+                    .build());
+        }
 
         return panelComponent.render(graphics);
     }
 
-    public void updateInventoryValue(final long newValue) {
-        SwingUtilities.invokeLater(() -> inventoryValue = newValue);
+
+    public void updateInventoryValue(final long newInventoryValue, final long newProfitInvValue, final long newProfitBankValue) {
+        final Color updateInvProfitColor = newProfitInvValue >= 0 ? Color.GREEN : Color.RED;
+        final Color updateBankProfitColor = newProfitBankValue >= 0 ? Color.GREEN : Color.RED;
+        SwingUtilities.invokeLater(() -> inventoryValue = newInventoryValue);
+        if(inventoryValueConfig.displayProfit()) {
+            SwingUtilities.invokeLater(() -> profitBankValue = newProfitBankValue);
+            SwingUtilities.invokeLater(() -> profitInvValue = newProfitInvValue);
+            SwingUtilities.invokeLater(() -> profitInvColor = updateInvProfitColor);
+            SwingUtilities.invokeLater(() -> profitBankColor = updateBankProfitColor);
+        }
     }
+
+
 }
